23f743b17462a3242a7ba088a2c63881d8e62030
gunasekhar9 <gunasekhar1539@gmail.com>
2022-03-22 09:32:09 +0530

    [gunasekhar9] add : Finally MVC is working fine

---
+++ BusinessLayer/Interface/IUserBL.cs
@@ -0,0 +1,16 @@
+﻿using ModelLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Interface
+{
+    public interface IUserBL
+    {
+        void addEmployee(Employeemodel employeemodel);
+        List<Employeemodel> GetEmployeemodels();
+        void updateEmployee(Employeemodel employeemodel);
+        void deleteEmployee(Employeemodel employeemodel);
+        Employeemodel GetEmployeemodel(int? id);
+    }
+}
---
+++ BusinessLayer/Services/UserBL.cs
@@ -0,0 +1,78 @@
+﻿using BusinessLayer.Interface;
+using ModelLayer.Services;
+using RepositoryLayer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    public class UserBL:IUserBL
+    {
+        IUserRL userRL;
+
+        public UserBL(IUserRL userRL)
+        {
+            this.userRL = userRL;
+        }
+        public void addEmployee(Employeemodel employeemodel)
+        {
+            try
+            {
+                 userRL.addEmployee(employeemodel);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void deleteEmployee(Employeemodel employeemodel)
+        {
+            try
+            {
+                 userRL.deleteEmployee(employeemodel);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Employeemodel GetEmployeemodel(int? id)
+        {
+            try
+            {
+                return userRL.GetEmployeemodel(id);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public List<Employeemodel> GetEmployeemodels()
+        {
+            try
+            {
+               return userRL.GetEmployeemodels();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void updateEmployee(Employeemodel employeemodel)
+        {
+            try
+            {
+                userRL.updateEmployee(employeemodel);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
